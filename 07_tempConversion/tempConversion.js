38a6fcfd8be0045b75aa1900ac337155bb1f2d92
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-04-06 14:34:19 -0400

    Complete 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * 5 / 9;
+  let rounded = Math.round(convertedTemp * 10)/10;
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = temp * 9 / 5 + 32;
+  let rounded = Math.round(convertedTemp * 10)/10;
+  return rounded
 };
 
 // Do not edit below this line

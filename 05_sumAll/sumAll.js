58067b960ae2b8093d86dad56380840ddacd909e
shirschfield <79327844+shirschfield@users.noreply.github.com>
2024-04-06 14:05:26 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
